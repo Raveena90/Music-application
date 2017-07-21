10c2a646071fac1b5a07d1a6479af6ed04949166
Raveena <raveena359@gmail.com>
2017-07-21 19:10:24 +0530

    MModified files

--- main.js
+++ main.js
@@ -1,13 +1,84 @@
-var currentSongNumber = 1;
-var willLoop = 0;
-var willShuffle = 0;
+//var currentSongNumber = 1;
+//var willLoop = 0;
+//var willShuffle = 0;
+//genre check
+			var relax=0;
+			var travel=0;
+			var party=0;
+			var work=0;
+			function toggleSong() {
+  
+								var song = document.querySelector('audio');
+								if(song.paused == true) {
+								console.log('Play');
+								$('.play-icon').removeClass('fa-play').addClass('fa-pause');
+								song.play();
+								}
+								else {
+								console.log('Pause');
+								$('.play-icon').removeClass('fa-pause').addClass('fa-play');
+								song.pause();
+								}
+								}
+								
+$('.welcome-screen button').on('click', function() {
+    var name = $('#name-input').val();
+    if (name.length > 2) {
+        var message = "Welcome, " + name;
+        $('.main .user-name').text(message);
+        $('.welcome-screen').addClass('hidden');
+        $('.mood').removeClass('hidden');
+    } else {
+          }    
+          $('#name-input').addClass('error');
+});
 
-var relax = 0;
-var travel = 0;
-var party = 0;
-var work = 0;
+					$('.mood ').on('click', function() {
+					  $('.mood').addClass('hidden');
+					  $('.main').removeClass('hidden');
+					});
+					$('.play-icon').on('click', function() {
+					  toggleSong();
 
-			var songs = [{
+					});
+					$('body').on('keypress', function(event) {
+					  var target = event.target;
+								if (event.keyCode == 32 &&target.tagName !='INPUT') {
+								toggleSong();
+							  }
+							});
+							
+function fancyTimeFormat(time)
+{
+// Hours, minutes and seconds
+var hrs = ~~(time / 3600);
+var mins = ~~((time % 3600) / 60);
+var secs = time % 60;
+
+// Output like "1:01" or "4:03:59" or "123:03:59"
+var ret = "";
+
+if (hrs > 0) {
+    ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+}
+
+ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+ret += "" + secs;
+return ret;
+}
+								function updateCurrentTime() { 
+						var song = document.querySelector('audio');
+						// console.log(song.currentTime);
+						// console.log(song.duration);
+						var currentTime = Math.floor(song.currentTime);
+						currentTime = fancyTimeFormat(currentTime);
+						var duration = Math.floor(song.duration);
+						duration = fancyTimeFormat(duration);
+						$('.time-elapsed').text(currentTime);
+						$('.song-duration').text(duration);
+						}
+
+var songs = [{
 					'name': 'Tamma Tamma',
 					'artist': 'Neha Kakkar, Monali Thakur, Ikka Singh, Dev Negi',
 					'album': 'Badrinath ki Dulhania',
@@ -44,7 +115,7 @@ var work = 0;
 				   'name': 'Four Five Seconds',
 				   'artist': 'Kanye West, Rihanna',
 				   'album': 'Four five seconds',
-				   'duration': '3:10',
+				   'duration': '3:08',
 				   'fileName': 'song5.mp3',
 				   'image' : 'song5.jpg'
 				},
@@ -142,216 +213,144 @@ var work = 0;
 					'image' : 'song16.jpg'
 				}]	
 				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
 
- function fancyTimeFormat(time)
-{   
-				// Hours, minutes and seconds
-				var hrs = ~~(time / 3600);
-				var mins = ~~((time % 3600) / 60);
-				var secs = time % 60;
 
-				// Output like "1:01" or "4:03:59" or "123:03:59"
-				var ret = "";
-				if (hrs > 0) {
-					ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
-				}
-				ret += "" + mins + ":" + (secs < 10 ? "0" : "");
-				ret += "" + secs;
-				return ret;
-				
+
+					function changeCurrentSongDetails(songObj) {
+					$('.current-song-image').attr('src',songObj.image)
+					$('.current-song-name').text(songObj.name)
+					$('.current-song-album').text(songObj.album)
+					}
+
+function addSongNameClickEvent(songObj,position) {
+  var songName = songObj.fileName;
+ var id = '#song' + position;
+ $(id).click(function() {
+ var audio = document.querySelector('audio');
+ var currentSong = audio.src;
+ if(currentSong.search(songName) != -1)
+  {
+   toggleSong();
+   changeCurrentSongDetails(songObj);
+  }
+ else {
+   audio.src = songName;
+   toggleSong();
+   changeCurrentSongDetails(songObj);
+   $('#now-playing').removeClass('main');
+   setTimeout(function(){
+     $('#now-playing').addClass('main');
+   },);
+    }
+  });
 }
 
+						function whichPlaylist(){ 
+							if(relax == 1){
 
-				 function updateTimer(){
-					 var song = document.querySelector('audio');
-					 var ct = song.currentTime;
-					 var td = song.duration;
-					 var percentage = (ct/td)*100;
-					 $('.progress-filled').css('width', percentage+ "%");
-				 }
-				 
-	 
-	 
-	 function toggleSong() {
-		var song = document.querySelector('audio');
-	 if(song.paused == true) {
-		console.log('Playing');
-		$('.play-icon').removeClass('fa-play').addClass('fa-pause');
-		song.play();
-		}
-	 else {
-		console.log('Pausing');
-		$('.play-icon').removeClass('fa-pause').addClass('fa-play');
-		song.pause();
-		}
-		} 
-		
-				 function changeCurrentSongDetails(songObj) {
-							   $('.current-song-image').attr('src','img/' + songObj.image)
-							   $('.current-song-name').text(songObj.name)
-							   $('.current-song-album').text(songObj.album)
-					                                         }
+								songs = songs_relax;
+							}
+							else if(travel == 1){
+								console.log("travel run");
+								songs = songs_travel ;
+							}
+							else if(party == 1){
+								songs = songs_party ;
+							}
+							else if( work== 1){
+								songs = songs_work ;
+							}
+							return songs;
+						}
 
-		
-      	function updateCurrentTime() {
-			var song = document.querySelector('audio');
-			//console.log(song.currentTime);
-			//console.log(song.duration);
-			var currentTime = Math.floor(song.currentTime);
-			currentTime = fancyTimeFormat(currentTime);
-			var duration = Math.floor(song.duration);
-			duration = fancyTimeFormat(duration)
-			$('.time-elapsed').text(currentTime);
-			$('.song-duration').text(duration);
-			}
-			
-			
-		function addSongNameClickEvent(songObj,position) {
-			var songName = songObj.fileName; 
-			var id = '#song' + position;
-			$(id).click(function() {
-			var audio = document.querySelector('audio');
-			var currentSong = audio.src;
-		if(currentSong.search(songName) != -1)
-			{
-			toggleSong();
-			}
-		else {
-			audio.src = songName;
-			toggleSong();
-			changeCurrentSongDetails(songObj); // Function Call
-			}
-			});
-			}
-			
-						window.onload = function() {
-						changeCurrentSongDetails(songs[0]);
-						updateCurrentTime();
-						setInterval(function() {
-						updateCurrentTime();
-						updateTimer();
-						},1000);
-						
-			//var songName1 = 'Tamma Tamma';
-			//var songName2 = 'Humma Song';
-			//var songName3 = 'Nashe Si Chadh Gayi';
-			//var songName4 = 'The Breakup Song';
-	
-			$('#songs').dataTable({
-            paging: false
-            });
-			}	
 
-						$('.fa-repeat').on('click',function(){
-						$('.fa-repeat').toggleClass('disabled')
-						willLoop = 1-willLoop;
+function updateSongList(){
+    songs=whichPlaylist();
+    console.log(songs[0]);
+    for(var i =0; i < songs.length;i++) {
+       var obj = songs[i];
+       var name = '#song' + (i+1);
+       var song = $(name);
+
+
+       song.find('.song-name').text(obj.name);
+       song.find('.song-artist').text(obj.artist);
+       song.find('.song-album').text(obj.album);
+       song.find('.song-length').text(obj.duration);
+       addSongNameClickEvent(obj,i+1)
+     }
+
+     $('#songs').DataTable(
+       {
+           paging: false,
+		     searching: false
+       }
+     );
+}
+
+							$('#relax').on('click',function(){
+							   if(typeof songs_table != 'undefined' ){
+								  songs_table.destroy();
+							   }
+								relax=1;
+								travel=party=work=0;
+								updateSongList();
+								$('.mood-sorting').addClass('hidden');
+								$('.content').removeClass('hidden');
+
 							});
-							$('.fa-random').on('click',function(){
-						$('.fa-random ').toggleClass('disabled')
-						willShuffle=1-willShuffle;
-							});
-																		
-											$('audio').on('ended',function(){
-											var audio = document.querySelector('audio');
-												if(currentSongNumber<4){
-														var nextSongObj=songs[currentSongNumber];
-														audio.src=nextSongObj.filename;
-														toggleSong();
-														changeCurrentSongDetails(nextSongObj);
-														currentSongNumber=currentSongNumber +1;
-													}
-												else{
-														$('.play-icon').removeClass('fa-pause').addClass('fa-play');
-														audio.currentTime=0;
-													}
-												})  
-	
-		  //for (var i = 0; i < fileNames.length ; i++) {
-	      //addSongNameClickEvent(fileNames[i],i)
-				
-	//}			
-		//window.onload = function() {
-			
-			$('#mood-relax').on('click',function(){
-				if(typeof songs_table != 'undefined'){
-					songs_table.destroy();
-				}
-				relax=1;
-				
-				updateSongList();
-				$('.mood-sorting').addClass('hidden');
-				$('.content').removeClass('hidden');
-				$('.content').addClass('animated   ')
-			});
-			
-			
-			
-		    for(var i =0; i < songs.length;i++) {
-			var obj = songs[i];
-			var name = '#song' + (i+1);
-			var song = $(name);
-			song.find('.song-name').text(obj.name);
-			song.find('.song-artist').text(obj.artist);
-			song.find('.song-album').text(obj.album);
-			song.find('.song-length').text(obj.duration);
-			addSongNameClickEvent(obj,i+1);
-	}
-			//addSongNameClickEvent(fileNames[0],1);
-			//addSongNameClickEvent(fileNames[1],2);
-			//addSongNameClickEvent(fileNames[2],3);
-			//addSongNameClickEvent(fileNames[3],4);
 
-			//for (var i = 0; i < fileNames.length ; i++) {
-			//addSongNameClickEvent(fileNames[i],i+1)
-	        //}
-					
-	
-		$('.welcome-screen button').on('click', function() {
-        var name = $('#name-input').val();
-	/*condition ke true hone pr msg aata*/
-        if (name.length > 2) {
-            var message = "Welcome, " + name; 
-			$('.main .user-name').text(message);
-	
-            $('.welcome-screen').addClass('hidden');
-            $('.main').removeClass('hidden');
-        } else {
-	/*Condition ke false hone pr red color se boundary aa jaati*/
-            $('#name-input').addClass('error');
-        }
-    });
-															
-    $('.play-icon').on('click', function() {
-        toggleSong(); 
-    });
-	
-	/*Keypress aisa event hai jise hum space press krte he song ko play pause kr skte*/
-         $('body').on('keypress', function(event) {
-	//32 means clicking on space bar
-	 var target = event.target;
-                if (event.keyCode == 32 && target.tagName !='INPUT') 
-				{
-                  toggleSong();   
-				  }
-            });
-	
-			
+
+							$('#travel').on('click',function(){
+								   if(typeof songs_table != 'undefined'){
+									songs_table.destroy();
+								}
+								travel=1;
+								party=work=relax=0;
+								updateSongList();
+								$('.mood').addClass('hidden');
+								$('.content').removeClass('hidden');
+
+							});
+
+							$('#party').on('click',function(){
+								 if(typeof songs_table != 'undefined'){
+								   songs_table.destroy();
+								}
+								party=1;
+								work=relax=travel=0;
+								updateSongList();
+								$('.mood').addClass('hidden');
+								$('.content').removeClass('hidden');
+
+							});
+
+
+							$('#work').on('click',function(){
+								   if(typeof songs_table != 'undefined'){
+									songs_table.destroy();
+								}
+								work=1;
+								relax=travel=party=0;
+								updateSongList();
+								$('.mood').addClass('hidden');
+								$('.content').removeClass('hidden');
+
+							});
+$('#back').on('click',function(){
+
+    $('.content').addClass('hidden');
+    $('.mood').removeClass('hidden');
+    
+$('.main').addClass('hidden');
+
+    song = document.querySelector('audio');
+          song.pause();
+
+  })
+
+window.onload = function() {
+setInterval(function(){
+updateCurrentTime();
+},1000);
+}
